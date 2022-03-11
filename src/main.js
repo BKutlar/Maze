14fc3b48197a23091630ebdd438f4cdba68731dc
Brayan Kutlar <brayan.kutlar@rdiup.com>
2022-03-11 15:21:24 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -3,18 +3,44 @@
 import {readFile} from 'fs';
 
 function main() {
-    console.log('Hello world', process.argv);
+    // console.log('Hello world', process.argv);
     readFile('maps/rect_01.map', "UTF-8", (err, data) => {
         if (err) throw err;
         const lines = data.split('\n');
-        for(let i=0; i < lines.length; i++) {
-            const line = lines[i].split(' ');
-            for(let j=0; j < lines.length; j++) {
-                matrices[j][i] = line[i];
+        // for(let i=0; i < lines.length; i++) {
+        //     const line = lines[i].split(' ');
+        //     for(let j=0; j < line.length; j++) {
+
+        //         // matrices[j][i] = lines[i];
+        //     }
+        // }
+        
+        const matrice = [];
+        let startX;
+        let startY;
+        let endX;
+        let endY;
+        for(let x = 0; x<lines.length; x++) {
+            if(!matrice[x]) {
+                matrice[x] = [];
             }
+            for(let y=0; y<lines[x].split('').length; y++) {
+                matrice[x][y] = lines[x][y];
+                
+              if(lines[x][y] === '1') {
+                startX = x;
+                startY = y;
+              }
+
+              if(lines[x][y] === '2') {
+                endX = x;
+                endY = y;
+              }
+            }
+            
         }
         
-        console.log(lines);
+        console.log(matrice,startX, startY, endX, endY);
     });
 
 }
