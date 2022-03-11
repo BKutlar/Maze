777352ee6291cc1adffa32fae4dedacf932249d0
Brayan Kutlar <brayan.kutlar@rdiup.com>
2022-03-11 12:05:48 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('Hello world', process.argv);
+// console.log('Hello world', process.argv);
+
+import {readFile} from 'fs';
+
+function main() {
+    console.log('Hello world', process.argv);
+    readFile('maps/rect_01.map', "UTF-8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
+
+}
+
+
+main();
