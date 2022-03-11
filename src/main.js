fa41abbc813b91d9fd17abbeffe87ec15b8c1be3
ibrahimaberete <47101718+ibrahimaberete@users.noreply.github.com>
2022-03-11 16:06:59 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ function main() {
     // console.log('Hello world', process.argv);
     readFile('maps/rect_01.map', "UTF-8", (err, data) => {
         if (err) throw err;
-        const lines = data.split('\n');
+        const lines = data.split('\r\n');
         // for(let i=0; i < lines.length; i++) {
         //     const line = lines[i].split(' ');
         //     for(let j=0; j < line.length; j++) {
@@ -18,6 +18,8 @@ function main() {
         let startY;
         let endX;
         let endY;
+        let curentX;
+        let curentY;
         for(let x = 0; x<lines.length; x++) {
             if(!matrice[x]) {
                 matrice[x] = [];
@@ -28,6 +30,8 @@ function main() {
               if(lines[x][y] === '1') {
                 startX = x;
                 startY = y;
+                curentX = x;
+                curentY = y;
               }
 
               if(lines[x][y] === '2') {
@@ -40,6 +44,19 @@ function main() {
         
         console.log(matrice,startX, startY, endX, endY);
     });
+    let isFound= false;
+    while(isFound){
+        matrice[curentX - 1][curentY] === '2' || ' ';
+        matrice[curentX + 1][curentY] === '2' || ' ';
+        matrice[curentX ][curentY-1] === '2' || ' ';
+        matrice[curentX ][curentY +1] === '2' || ' ';
+
+
+
+        continue;
+    }
+
+
 
 }
 
